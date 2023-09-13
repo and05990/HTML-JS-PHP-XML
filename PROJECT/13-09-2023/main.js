d1ce63046bfbca1b4b86dd20da1a313d7834a648
and05990 <104375703+and05990@users.noreply.github.com>
2023-09-13 11:49:37 +0200

    Add files via upload

---
+++ PROJECT/13-09-2023/main.js
@@ -0,0 +1,17 @@
+const surname     = document.getElementById("surname");
+const name        = document.getElementById("name");
+const date        = document.getElementById("date");
+const sex         = document.getElementById("sex");
+const city        = document.getElementById("city");
+
+const btnGenerate = document.getElementById("generate");
+
+btnGenerate.function(event => onclick)
+{
+
+}
+
+function separateConsonats()
+{
+    let str = 
+}
