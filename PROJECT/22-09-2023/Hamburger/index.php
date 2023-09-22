27f8445029d53497718125e0db77224d202458b3
and05990 <104375703+and05990@users.noreply.github.com>
2023-09-22 21:13:11 +0200

    Update index.php

--- PROJECT/22-09-2023/Hamburger/index.php
+++ PROJECT/22-09-2023/Hamburger/index.php
@@ -1,7 +1,42 @@
 <?php
     if (isset($_POST["carne"]) || isset($_POST["salsa"]) || isset($_POST["cetriolini"]) || isset($_POST["cipolla"]) || isset($_POST["insalata"]) || isset($_POST["formaggio"]) || isset($_POST["pomodori"]))
     {
-        echo "Ordine effettuato";
+        echo "<h1>Il tuo panino è composto da:</h1>";
+        if (isset($_POST["carne"]))
+        {
+            echo "Carne<br>";
+        }
+
+        if (isset($_POST["salsa"])) 
+        {
+            echo "Salsa<br>";
+        }
+
+        if (isset($_POST["cetriolini"])) 
+        {
+            echo "Cetriolini<br>";
+        }
+
+        if (isset($_POST["cipolla"]))
+        {
+            echo "Cipolla<br>";
+        }
+
+        if (isset($_POST["insalata"])) 
+        {
+            echo "Insalata<br>";
+        }
+
+        if (isset($_POST["formaggio"])) 
+        {
+            echo "Formaggio<br>";
+        }
+
+        if (isset($_POST["pomodori"])) 
+        {
+            echo "Pomodori<br>";
+        }
+        
     } else {
         header("Location: main.php");
         exit;
