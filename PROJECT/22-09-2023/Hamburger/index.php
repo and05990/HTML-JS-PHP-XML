b4dde509d13674c3e53ad9ecaa688fa393f649d6
and05990 <104375703+and05990@users.noreply.github.com>
2023-09-22 12:49:50 +0200

    Delete PROJECT/22-09-2023 directory

--- PROJECT/22-09-2023/Hamburger/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (isset($_POST["carne"]) || isset($_POST["salsa"]) || isset($_POST["cetriolini"]) || isset($_POST["cipolla"]) || isset($_POST["insalata"]) || isset($_POST["formaggio"]) || isset($_POST["pomodori"]))
-    {
-        echo "Ordine effettuato";
-    } else {
-        header("Location: main.php");
-        exit;
-    }
-?>
