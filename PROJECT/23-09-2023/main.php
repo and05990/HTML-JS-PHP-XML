8d5c65d757ee8dd01b722788a172d46f2ecaff69
and05990 <104375703+and05990@users.noreply.github.com>
2023-09-23 20:07:03 +0200

    Add files via upload

---
+++ PROJECT/23-09-2023/main.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data anagrafics</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Are you gay?</h1>
+    <form action="index.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" placeholder="Name">
+        <input type="text" name="surname" placeholder="Surname">
+        <input type="date" name="date">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
