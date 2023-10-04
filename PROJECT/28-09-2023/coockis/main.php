2fdfc2c37dd3858e5ceedf668c91d84a75bf6613
and05990 <104375703+and05990@users.noreply.github.com>
2023-10-04 10:03:17 +0200

    Delete PROJECT directory

--- PROJECT/28-09-2023/coockis/main.php
+++
@@ -1,9 +0,0 @@
-<?php
-    setcookie("prova", "prova", strtotime("+30 days"));
-    if(isset($_COOKIE["prova"]))
-    {
-        echo $_COOKIE["prova"];
-    } else {
-        echo "<p>Non trovato</p>";
-    }
-?>
