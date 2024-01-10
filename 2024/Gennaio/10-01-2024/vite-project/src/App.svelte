7854a3bee7a7fefadca4a62b187d3ffc3362a95f
and05990 <104375703+and05990@users.noreply.github.com>
2024-01-10 10:51:10 +0000

    sndkjsfndskj

---
+++ 2024/Gennaio/10-01-2024/vite-project/src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  let importo = 0;
+  let saldo = 10000;
+    function preleva() {
+        saldo = saldo - importo;
+    }
+</script>
+
+<main>
+  <h1>Benvenuto su ATM!</h1>
+  <div class="atm">
+    <h2>Preleva</h2>
+    <hr>
+    <h3>Saldo: {saldo}€</h3>
+    <p>Selezionarel'importo da prelevare:
+    <input type="range" name="importo" bind:value={importo} min="1" max="{saldo}"> €
+    <output for="importo">{importo}</output> </p>
+    <hr>
+    <button class="preleva" on:click={preleva}>Preleva</button>
+  </div>
+</main>
+
+<style>
+  .preleva{
+    background-color: rgb(245, 75, 75);
+  }
+
+  .atm{
+    border: 1px solid black;
+    padding: 20px;
+    border-radius: 10px;
+    width: 300px;
+    margin: 0 auto;
+  }
+</style>
