81a7ab944649e9ecabf1c830b3245d9fdc5a5d29
and05990 <104375703+and05990@users.noreply.github.com>
2024-01-10 18:18:14 +0100

    Update App.svelte

--- 2024/Gennaio/10-01-2024/vite-project/src/App.svelte
+++ 2024/Gennaio/10-01-2024/vite-project/src/App.svelte
@@ -1,23 +1,47 @@
 <script>
-  let importo = 0;
+  let importo = 1;
+  let importoD = 1;
   let saldo = 10000;
     function preleva() {
+      if (saldo > 0)
         saldo = saldo - importo;
+      else
+        alert("Non hai abbastanza soldi!");
+    }
+
+    function deposita() {
+      saldo = saldo + importoD; 
     }
 </script>
 
 <main>
-  <h1>Benvenuto su ATM!</h1>
+  <h1>Benvenuto sul tuo ATM!</h1>
   <div class="atm">
-    <h2>Preleva</h2>
-    <hr>
-    <h3>Saldo: {saldo}€</h3>
-    <p>Selezionarel'importo da prelevare:
-    <input type="range" name="importo" bind:value={importo} min="1" max="{saldo}"> €
-    <output for="importo">{importo}</output> </p>
-    <hr>
-    <button class="preleva" on:click={preleva}>Preleva</button>
+    <div class="prelievo">
+      <h2>Preleva</h2>
+      <hr>
+      <h3>Saldo: {saldo}€</h3>
+      <p>Selezionarel'importo da prelevare:
+        <input type="range" name="importo" bind:value={importo} min="1" max="{saldo}"> €
+        <output for="importo">{importo}</output>
+      </p>
+      <hr>
+      <button class="preleva" on:click={preleva}>Preleva</button>
+    </div>
+    <div class="deposita">
+      <h2>Deposita</h2>
+      <hr>
+      <h3>Saldo: {saldo}€</h3>
+      <p>Selezionarel'importo da depositare:
+        <input type="range" name="importoD" bind:value={importoD} min="1" max="100000"> €
+        <output for="importoD">{importoD}</output> 
+      </p>
+      <hr>
+      <button class="deposito" on:click={deposita}>Deposita</button>
+    </div>
   </div>
+
+  <footer> <p><a href="https://github.com/and05990">GitHub</a></p> </footer>
 </main>
 
 <style>
@@ -25,11 +49,31 @@
     background-color: rgb(245, 75, 75);
   }
 
-  .atm{
+  .deposito{
+    background-color: rgb(245, 75, 75);
+  }
+
+  .prelievo{
     border: 1px solid black;
     padding: 20px;
     border-radius: 10px;
     width: 300px;
     margin: 0 auto;
+    margin-right: 20px;
+    box-shadow: 5px 5px 5px grey;
+  }
+
+  .deposita{
+    border: 1px solid black;
+    padding: 20px;
+    border-radius: 10px;
+    width: 300px;
+    margin: 0 auto;
+    box-shadow: 5px 5px 5px grey;
+  }
+
+  .atm{
+    display: flex;
+    flex-direction: row;
   }
 </style>
