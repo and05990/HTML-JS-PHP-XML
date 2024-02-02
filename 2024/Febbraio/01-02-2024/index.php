abf13fee3f2fd056153bd0d194cbe0edfdb45a52
and05990 <104375703+and05990@users.noreply.github.com>
2024-02-02 12:36:41 +0100

    Add files via upload

--- 2024/Febbraio/01-02-2024/index.php
+++ 2024/Febbraio/01-02-2024/index.php
@@ -7,15 +7,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Portfolio Creator</title>
 </head>
 <body>
     <form action="portafolio.php" method="post">
         <input type="text" name="nome" placeholder="Nome">
         <input type="text" name="cognome" placeholder="Cognome">
         <input type="text" name="citta" placeholder="Città">
+        <input type="text" name="descrizione" placeholder="Descrizione">
+        <div class="testi">
+            <input type="text" class="t1" name="t1" placeholder="Testo">
+                <select name="type1" class="t1">
+                    <option value="<h1>">Titolo</option>
+                    <option value="<h2>">Sottotitolo</option>
+                    <option value="<p>">Testo</option>
+                </select>
+
+            <button name="addT">+</button>
+        </div>
         <input type="image" name="fotoProfilo" placeholder="Immagine profilo">
         <button type="submit">Vedi Ora</button>
     </form>
+
+    <script src="index.js"></script>
 </body>
 </html>
--- 2024/Febbraio/01-02-2024/portafolio.php
+++ 2024/Febbraio/01-02-2024/portafolio.php
@@ -1,10 +1,29 @@
 <?php
     session_start();
-    if(isset($_POST["nome"]) && isset($_POST["cognome"]))
+
+    if(isset($_POST["nome"]) && isset($_POST["cognome"]) && isset($_POST["citta"]) && isset($_POST["fotoProfilo"]) && isset($_POST["descrizione"]))
     {
         $nome = $_POST["nome"];
         $cognome = $_POST["cognome"];
-        
-        echo "<h1>".$nome."</h1>";
+        $citta = $_POST["citta"];
+        $fotoProfilo = $_POST["fotoProfilo"];
+        $descrizione = $_POST["descrizione"];
+
+        if(empty($nome) || empty($cognome) || empty($citta) || empty($fotoProfilo) || empty($descrizione))
+        {
+            header("Location: index.php");
+        } else {
+            $portNome = $nome;
+            $portCognome = $cognome;
+            $portCitta = $citta;
+            $portFotoProfilo = $fotoProfilo;
+            $portDescrizione = $descrizione;
+
+            
+        }
+    } else {
+        header("Location: index.php");
     }
 ?>
+
+
